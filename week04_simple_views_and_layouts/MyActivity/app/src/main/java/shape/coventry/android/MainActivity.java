48e4e5dd05be62257262d884f4e9506b16ff15e9
kwokyuhovtc <kwokyuho@vtc.edu.hk>
2020-02-10 14:34:27 +0800

    - add my activity in week 4

---
+++ week04_simple_views_and_layouts/MyActivity/app/src/main/java/shape/coventry/android/MainActivity.java
@@ -0,0 +1,26 @@
+package shape.coventry.android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
