24500e89a5cdc4aff94f0d62007ae28ee180cdfa
kwokyuhovtc <kwokyuho@vtc.edu.hk>
2020-04-16 20:34:44 +0800

    -add services and wearables.

---
+++ week12_services_and_wearables/MyBoundServices/app/src/main/java/com/example/jianhuayang/myboundservices/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.jianhuayang.myboundservices;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    LocalService mService;
+    boolean mBound = false;
+    /**
+     * Defines callbacks for service binding, passed to bindService()
+     */
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            LocalService.LocalBinder binder = (LocalService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Bind to LocalService
+        Intent intent = new Intent(this, LocalService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(mConnection);
+        mBound = false;
+    }
+
+    /**
+     * Called when a button is clicked (the button in the layout file attaches to
+     * this method with the android:onClick attribute)
+     */
+    public void onButtonClick(View v) {
+        if (mBound) {
+            // Call a method from the LocalService.
+            // However, if this call were something that might hang, then this request should
+            // occur in a separate thread to avoid slowing down the activity performance.
+            int num = mService.getRandomNumber();
+            Toast.makeText(this, "number: " + num, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
+
---
+++ week12_services_and_wearables/MyWearables2/mobile/src/main/java/com/example/jianhuayang/mywearables/ListenerService.java
@@ -0,0 +1,19 @@
+package com.example.jianhuayang.mywearables;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.wearable.MessageEvent;
+import com.google.android.gms.wearable.WearableListenerService;
+
+public class ListenerService extends WearableListenerService {
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        Log.d("DEBUG_KEY", "on received");
+        showToast(messageEvent.getPath());
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+}
